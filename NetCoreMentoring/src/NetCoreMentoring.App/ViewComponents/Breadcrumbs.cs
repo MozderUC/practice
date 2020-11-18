09e7f1a33149f4f8098ce286bc408d28243b6697
Uladzislau Mozhjer <Uladzislau_Mozhjer@epam.com>
2020-11-18 12:04:17 +0300

    NetCoreMentoring // add Breadcrumbs view component

---
+++ NetCoreMentoring/src/NetCoreMentoring.App/ViewComponents/Breadcrumbs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMentoring.App.ViewComponents
+{
+    public class Breadcrumbs : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string url)
+        {
+            var pathComponents = new List<string>() {"Home"};
+
+            pathComponents.AddRange(GetPathComponents(url));
+
+            return View("Default", pathComponents);
+
+        }
+
+        private IEnumerable<string> GetPathComponents(string url)
+        {
+            return url.StartsWith('/')
+                ? url.Substring(1).Split('/')
+                : url.Split('/');
+        }
+    }
+}
