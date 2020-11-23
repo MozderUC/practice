d3c409c49a67aac32a80509222963758c46742de
Uladzislau Mozhjer <Uladzislau_Mozhjer@epam.com>
2020-11-23 12:19:50 +0300

    NetCoreMentoring // add structure that implements the status of the operation

---
+++ NetCoreMentoring/src/NetCoreMentoring.Core/Utilities/ResultFlow/Error.cs
@@ -0,0 +1,19 @@
+﻿namespace NetCoreMentoring.Core.Utilities.ResultFlow
+{
+    public readonly struct Error
+    {
+        public Error(string message)
+            :this(message, null)
+        {
+        }
+
+        public Error(string message, object errorDetails)
+        {
+            this.Message = message;
+            this.ErrorDetails = errorDetails;
+        }
+
+        public string Message { get; }
+        public object ErrorDetails { get; }
+    }
+}
