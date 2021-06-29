a8ce7d21eebabf0b53685610e09f995cf81937a5
Uladzislau Mozhjer <Uladzislau_mozhjer@epam.com>
2021-06-29 12:24:28 +0300

    Codewars  // binary search algorithm; ValidParentheses kata

---
+++ Codewars/Kata.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    // Not Kata
+    // BinarySearch
+
+
+    // 6 kyi
+
+    // 5 kyi
+    /// ValidParentheses https://www.codewars.com/kata/52774a314c2333f0a7000688
+
+    // 4 kyi
+
+    // 3 kyi
+
+    // 2 kyi
+
+    // 1 kyi
+
+    public static bool BinarySearch(IEnumerable<int> sortedSet, int valueToSearch)
+    {
+        var sortedArray = sortedSet as int[] ?? sortedSet.ToArray();
+        return BinarySearchImpl(sortedArray, valueToSearch, 0, sortedArray.Length - 1);
+    }
+
+    private static bool BinarySearchImpl(int[] sortedArray, int valueToSearch, int startIndex, int endIndex)
+    {
+        if (endIndex - startIndex == 1)
+        {
+            return sortedArray[startIndex] == valueToSearch || sortedArray[endIndex] == valueToSearch;
+        }
+
+        var midIndex = startIndex + (endIndex - startIndex) / 2;
+
+        // take left part of array
+        if (sortedArray[midIndex] > valueToSearch)
+        {
+            return BinarySearchImpl(sortedArray, valueToSearch, startIndex, midIndex);
+        }
+
+        // take right part of array
+        if (sortedArray[midIndex] < valueToSearch)
+        {
+            return BinarySearchImpl(sortedArray, valueToSearch, midIndex, endIndex);
+        }
+
+        return true;
+    }
+
+    public static bool ValidParentheses(string input)
+    {
+        var stack = new Stack<char>();
+
+        foreach (var i in input)
+        {
+            if (i == '(')
+            {
+                stack.Push(i);
+            }
+
+            if (i == ')')
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                stack.Pop();
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
---
+++ Codewars/KataTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class KataTest
+{
+    [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 290)]
+    [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 1090)]
+    [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 0)]
+    public void BinarySearch_SearchValueThatDontExistInInput_False(int[] input, int valueToSearch)
+    {
+        Assert.That(Kata.BinarySearch(input , valueToSearch), Is.False);
+    }
+
+    [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 1)]
+    [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 1089)]
+    [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 107)]
+    public void BinarySearch_SearchValueThatExistInInput_True(int[] input, int valueToSearch)
+    {
+        Assert.That(Kata.BinarySearch(input , valueToSearch), Is.True);
+    }
+
+    [TestCase(")((((")]
+    [TestCase("()()()()()))(((()()()")]
+    [TestCase("((()))))")]
+    public void ValidParentheses_InvalidInput_False(string input)
+    {
+        Assert.That(Kata.ValidParentheses(input), Is.False);
+    }
+
+    [TestCase("()")]
+    [TestCase("(()()())")]
+    [TestCase("((()))")]
+    public void ValidParentheses_ValidInput_True(string input)
+    {
+        Assert.That(Kata.ValidParentheses(input), Is.True);
+    }
+}
