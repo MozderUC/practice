d7f259006f77a6305d23750c94ceb0d5d06aaff3
Uladzislau Mozhjer <Uladzislau_mozhjer@epam.com>
2021-07-05 11:39:01 +0300

    Codewars  // rename part of tests to the following: MethodNameThatTested_Scenario_ExpectedBehavior

--- Codewars/CodewarsDotNet/CodewarsCSharp/KataTests.cs
+++ Codewars/CodewarsDotNet/CodewarsCSharp/KataTests.cs
@@ -243,7 +243,7 @@ public class KataTest
     [TestCase(60, "00:01:00")]
     [TestCase(86399, "23:59:59")]
     [TestCase(359999, "99:59:59")]
-    public void GetReadableTime_ParseIntToTimeString_TimeString(int input, string result)
+    public void GetReadableTime_IntOfSeconds_ReadableTimeString(int input, string result)
     {
         Assert.That(Kata.GetReadableTime(input), Is.EqualTo(result));
     }
@@ -251,7 +251,7 @@ public class KataTest
     [TestCase(new []{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}, new []{1, 2, 1, 1, 3, 1, 0, 0, 0, 0})]
     [TestCase(new []{0, 0, 0, 0}, new []{0, 0, 0, 0})]
     [TestCase(new []{1, 0, 2, 0, 3, 0}, new []{1, 2, 3, 0, 0, 0})]
-    public void MoveZeroes_MoveZerosToTheEndOfArray_ArrayWithTheZerosInTheEnd(int[] input, int[] result)
+    public void MoveZeroes_ArrayThatCanContainZeros_ArrayWithTheZerosInTheEndOfArray(int[] input, int[] result)
     {
         Assert.That(Kata.MoveZeroes(input), Is.EqualTo(result));
     }
@@ -259,7 +259,7 @@ public class KataTest
     [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 290)]
     [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 1090)]
     [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 0)]
-    public void BinarySearch_SearchValueThatDontExistInInput_False(int[] input, int valueToSearch)
+    public void BinarySearch_InputNotContainValueToSearch_False(int[] input, int valueToSearch)
     {
         Assert.That(Kata.BinarySearch(input , valueToSearch), Is.False);
     }
@@ -267,7 +267,7 @@ public class KataTest
     [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 1)]
     [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 1089)]
     [TestCase(new []{1, 2, 5, 6, 8, 17, 18, 21, 22, 107, 289, 1089}, 107)]
-    public void BinarySearch_SearchValueThatExistInInput_True(int[] input, int valueToSearch)
+    public void BinarySearch_InputContainValueToSearch_True(int[] input, int valueToSearch)
     {
         Assert.That(Kata.BinarySearch(input , valueToSearch), Is.True);
     }
