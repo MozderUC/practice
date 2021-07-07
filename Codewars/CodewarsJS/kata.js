e53fbcd56e91634399171f24735fc8578561abe7
Uladzislau Mozhjer <Uladzislau_mozhjer@epam.com>
2021-07-07 22:49:42 +0300

    Codewars  // arrayDiff; countSmileys; phone; score

--- Codewars/CodewarsJS/kata.js
+++ Codewars/CodewarsJS/kata.js
@@ -3,8 +3,13 @@
 
 // 6 kyi
 // uniqueInOrder https://www.codewars.com/kata/54e6533c92449cc251001667
+// iqTest https://www.codewars.com/kata/552c028c030765286c00007d
+// arrayDiff https://www.codewars.com/kata/523f5d21c841566fde000009
+// countSmileys https://www.codewars.com/kata/583203e6eb35d7980400002a
 
 // 5 kyi
+// phone https://www.codewars.com/kata/56baeae7022c16dd7400086e
+// score https://www.codewars.com/kata/5270d0d18625160ada0000e4
 
 // 4 kyi
 
@@ -13,6 +18,106 @@
 // 2 kyi
 
 // 1 kyi
+
+exports.score = function (diceRolls) {
+    var result = 0;
+    var tripleMatchScore = {
+        1: 1000,
+        2: 200,
+        3: 300,
+        4: 400,
+        5: 500,
+        6: 600
+    };
+    var oneMatchScore = {
+        1: 100,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 50,
+        6: 0,
+    };
+
+    var groupedDiceRolls = {};
+    diceRolls.forEach(d => { if (d in groupedDiceRolls) groupedDiceRolls[d]++; else groupedDiceRolls[d] = 1; });
+
+    for (var group in groupedDiceRolls) {
+        if (groupedDiceRolls[group] >= 3){
+            result += tripleMatchScore[group];
+            groupedDiceRolls[group] -= 3;
+        }
+        if (groupedDiceRolls[group] == 1){
+            result += oneMatchScore[group];
+            groupedDiceRolls[group] -= 1;
+        }
+    }
+
+    return result;
+}
+
+exports.phone = function (phoneDirectory, phoneNumber) {
+    var phoneDirectoryArr = phoneDirectory.split('\n');
+
+    // google this options of regular expressions to understand this patterns: lookahead, lookbehind, non-greedy/greedy 
+    var phoneNumberPattern = /(?<=\+)[\d-]+/g;
+    var nameWithoutBracketsPattern = /(?<=\<).+?(?=\>)/g;
+    var isTrashPattern = /(\s|^)([^A-Za-z0-9])+(\s|$)/g;
+
+    // filter arr to select only elements with phone == phoneNumber
+    var targetRecords = phoneDirectoryArr.filter(x => {
+        var matchedPhones = x.match(phoneNumberPattern);
+        if(matchedPhones == null || matchedPhones.length != 1)
+            return false;
+        
+        if (matchedPhones[0] == phoneNumber)
+            return true;
+        
+        return false;
+    })
+
+    // validation
+    if (targetRecords.length > 1){
+        return 'Error => Too many people: ' + phoneNumber;
+    }
+
+    if (targetRecords.length == 0){
+        return 'Error => Not found: ' + phoneNumber;
+    }
+
+    var name = targetRecords[0].match(nameWithoutBracketsPattern);
+    var address = targetRecords[0]
+        .replace(nameWithoutBracketsPattern, '')
+        .replace(phoneNumberPattern, '')
+        .replace(/\s+|_/g, ' ')
+        .split(' ')
+        .map(x => {
+            if(!x)
+                return;
+            if (x.match(isTrashPattern))
+                return;
+            if (x[0].match(/[^A-Za-z0-9]/))
+                x = x.substring(1,x.length);
+            if (x[x.length - 1] != '.' && x[x.length - 1].match(/[^A-Za-z0-9]/))
+                x = x.substring(0, x.length - 1);
+            return x;
+        })
+        .join(' ')
+        .replace(/\s+/g, ' ')
+        .trim();
+        
+    return "Phone => " + phoneNumber + ", Name => " + name + ", Address => " + address;
+}
+
+exports.countSmileys = function (arr) {
+    return arr.filter(x => 
+        (x[0] == ':' || x[0] == ';')
+        && (x.length == 2 || (x.length == 3 && (x[1] == '-' || x[1] == '~')))
+        && (x[x.length - 1] == ')' || x[x.length - 1] == 'D')).length;
+}
+
+exports.arrayDiff = function (a, b) {
+    return [...a].filter(x => [...b].indexOf(x) == -1);
+}
 
 exports.iqTest = function (numbers) {
 
--- Codewars/CodewarsJS/kata.test.js
+++ Codewars/CodewarsJS/kata.test.js
@@ -1,6 +1,52 @@
 var kata = require('./kata.js');
 
-test('iqTest_listOfNumbersWhereOneDiffInEvenness_indexOfThisNumber+1', () => {
+test('score_arrayOf5DiceRollResults_TotalScoreCalculatingByKataRules', () => {
+    expect(kata.score([1, 1, 1, 1, 3])).toEqual(1100);
+    expect(kata.score([2, 3, 4, 6, 2])).toEqual(0);
+    expect(kata.score([4, 4, 4, 3, 3])).toEqual(400);
+    expect(kata.score([2, 4, 4, 5, 4])).toEqual(450);
+});
+
+const phoneDirectory = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010\n"
++ "+1-541-984-3012 <P Reed> /PO Box 530; Pollocksville, NC-28573\n :+1-321-512-2222 <Paul Dive> Sequoia Alley PQ-67209\n"
++ "+1-741-984-3090 <Peter Reedgrave> _Chicago\n :+1-921-333-2222 <Anna Stevens> Haramburu_Street AA-67209\n"
++ "+1-111-544-8973 <Peter Pan> LA\n +1-921-512-2222 <Wilfrid Stevens> Wild Street AA-67209\n"
++ "<Peter Gone> LA ?+1-121-544-8974 \n <R Steell> Quora Street AB-47209 +1-481-512-2222\n"
++ "<Arthur Clarke> San Antonio $+1-121-504-8974 TT-45120\n <Ray Chandler> Teliman Pk. !+1-681-512-2222! AB-47209,\n"
++ "<Sophia Loren> +1-421-674-8974 Bern TP-46017\n <Peter O'Brien> High Street +1-908-512-2222; CC-47209\n"
++ "<Anastasia> +48-421-674-8974 Via Quirinal Roma\n <P Salinger> Main Street, +1-098-512-2222, Denver\n"
++ "<C Powel> *+19-421-674-8974 Chateau des Fosses Strasbourg F-68000\n <Bernard Deltheil> +1-498-512-2222; Mount Av.  Eldorado\n"
++ "+1-099-500-8000 <Peter Crush> Labrador Bd.\n +1-931-512-4855 <William Saurin> Bison Street CQ-23071\n"
++ "<P Salinge> Main Street, +1-098-512-2222, Denve\n"
+
+test('phone_phoneDirectoryAndOnePhoneNumber_formattedRecordForThatPhoneNumber', () => {
+    expect(kata.phone(phoneDirectory, '1-541-754-3010')).toEqual("Phone => 1-541-754-3010, Name => J Steeve, Address => 156 Alphand St.");
+    expect(kata.phone(phoneDirectory, '1-908-512-2222')).toEqual("Phone => 1-908-512-2222, Name => Peter O'Brien, Address => High Street CC-47209");
+    expect(kata.phone(phoneDirectory, '48-421-674-8974')).toEqual("Phone => 48-421-674-8974, Name => Anastasia, Address => Via Quirinal Roma");
+    expect(kata.phone(phoneDirectory, '1-921-512-2222')).toEqual("Phone => 1-921-512-2222, Name => Wilfrid Stevens, Address => Wild Street AA-67209");
+    expect(kata.phone(phoneDirectory, '1-121-504-8974')).toEqual("Phone => 1-121-504-8974, Name => Arthur Clarke, Address => San Antonio TT-45120");
+    expect(kata.phone(phoneDirectory, '1-498-512-2222')).toEqual("Phone => 1-498-512-2222, Name => Bernard Deltheil, Address => Mount Av. Eldorado");
+    expect(kata.phone(phoneDirectory, '1-098-512-2222')).toEqual("Error => Too many people: 1-098-512-2222");
+    expect(kata.phone(phoneDirectory, '5-555-555-5555')).toEqual("Error => Not found: 5-555-555-5555");
+});
+
+test('countSmileys_arrayWithSmiles_countOfHappySmiles', () => {
+    expect(kata.countSmileys([';~~D'])).toBe(0);
+    expect(kata.countSmileys([])).toBe(0);
+    expect(kata.countSmileys([':D',':~)',';~D',':)'])).toBe(4);
+    expect(kata.countSmileys([':)',':(',':D',':O',':;'])).toBe(2);
+    expect(kata.countSmileys([';]', ':[', ';*', ':$', ';-D'])).toBe(1);
+});
+
+test('arrayDiff_twoArrays_firstArrayElements-secondArrayElements', () => {
+    expect(kata.arrayDiff([], [4,5])).toEqual([]);
+    expect(kata.arrayDiff([3,4], [3])).toEqual([4]);
+    expect(kata.arrayDiff([1,8,2], [])).toEqual([1,8,2]);
+    expect(kata.arrayDiff([1,2,3], [1,2])).toEqual([3]);
+    expect(kata.arrayDiff([1,2,2,2,3],[2])).toEqual([1,3]);
+});
+
+test('iqTest_listOfNumbersWhereOneDiffInEvenness_indexOfThisOne+1', () => {
     expect(kata.iqTest("2 4 7 8 10")).toBe(3);
     expect(kata.iqTest("1 2 2")).toBe(1);
 });
